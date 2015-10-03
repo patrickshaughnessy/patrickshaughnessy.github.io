08074925c6a2c62e7f2c8179ff125bc9f3ad7dc5
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-10-02 20:06:31 -0700

    Updated site structure and added gitignore file

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $(window).scrollTo($('#title'));
+  return;
+});
+
+var me = {
+  firstName: 'Patrick',
+  lastName: 'Shaughnessy',
+  language: 'javascript'
+};
+
+me.welcome = function(){
+  var $yourName = $('#yourName').val();
+  console.log("Hello " + $yourName + "! My name is " + this.firstName + " " + this.lastName + ", and I'm a " + this.language + " developer.");
+  var welcomeMessage = "My name is " + this.firstName + " " + this.lastName + ", and I'm a " + this.language + " developer.";
+  return welcomeMessage;
+};
+
+$('#yourName').keypress(function(e){
+  if (e.which == 13) {
+    var welcomeMessage = me.welcome();
+    $('#welcomeMessage').text(welcomeMessage);
+    $(window).scrollTo($('#welcome'), 800, {easing:'easeOutCirc'});
+
+    return false;
+  }
+});
+
+$('a').click(function(e){
+  e.preventDefault();
+  var loc = this.hash;
+  console.log(loc);
+  $(window).scrollTo($(loc), 800, {easing: 'easeOutCirc'});
+  return false;
+})
